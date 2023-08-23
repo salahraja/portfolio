3ac23ac17a771e9843145b5c5387c708d14caedf
salahraja <53281053+salahraja@users.noreply.github.com>
2023-08-23 10:05:33 -0500

    fixed favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,21 +4,22 @@ import Head from "next/head";
 import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
-  <>
-    {/* Add the favicon */}
-    <Head>
-      <link rel="shortcut icon" href="https://ln5.sync.com/dl/a699f5400/b9iq6q48-safke7sv-d5ta2cef-s4nerx2n" />
-      <title>My page title</title>
-      <meta property="og:title" content="Salah's Portfolio" />
+  return (
+    <>
+      {/* Add the favicon */}
+      <Head>
+        <link rel="shortcut icon" href="https://ln5.sync.com/dl/a699f5400/b9iq6q48-safke7sv-d5ta2cef-s4nerx2n" />
+        <title>My page title</title>
+        <meta property="og:title" content="Salah's Portfolio" />
         <meta property="og:description" content="My coding portfolio!" />
         <meta property="og:image" content="https://ln5.sync.com/dl/7d729f030/fz2kbahu-ic82gbfv-5ne52xe9-zq8u9xpn" />
         <meta property="og:url" content="https://www.salahs.org" />
-    </Head>
-    {/* Add the favicon */}
-    {/* Note that the path doesn't include "public" */}
-    
-    <Component {...pageProps} />
-  </>;
-  return <Component {...pageProps} />;
-  <Analytics />
+      </Head>
+      {/* Add the favicon */}
+      {/* Note that the path doesn't include "public" */}
+      
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
